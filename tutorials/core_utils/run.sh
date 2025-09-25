c24514108aed26149cb58ae52cb58b286b8ae529
dyingc <dyingc@hotmail.com>
2025-09-24 22:20:52 -0700

    Add original echo source and update run script for modified product

--- tutorials/core_utils/run.sh
+++ tutorials/core_utils/run.sh
@@ -1,20 +1,21 @@
 VER=6.11
 PROD="echo"
+MODIFIED_PROD="${PROD}_challenge2"
 
 docker run --rm -it -e DISPLAY=:1 \
     --ulimit='stack=-1:-1' \
     -e LLVM_COMPILER=clang \
     -w /home/klee/coreutils-${VER}/obj-llvm/src \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $(pwd)/${PROD}_challenge2.c:/home/klee/coreutils-${VER}/src/${PROD}.c:ro \
+    -v $(pwd)/${MODIFIED_PROD}.c:/home/klee/coreutils-${VER}/src/${MODIFIED_PROD}.c:ro \
     -v $(pwd)/rerun_asan.py:/home/klee/coreutils-${VER}/obj-llvm/src/rerun_asan.py:ro \
     klee-coreutils:${VER} \
     bash -lc \
     "\
-    cd ..; make -j$(nproc) -C src ${PROD}; cd -; \
-    echo '✅ compile for Ordinary ${PROD}: done'; \
-    clang -I ../../include -fsanitize=address -g -O0 /home/klee/coreutils-${VER}/src/${PROD}.c -o /home/klee/coreutils-${VER}/obj-llvm/src/${PROD}_asan; \
-    echo '✅ compile for ASan ${PROD}: done'; \
-    extract-bc ${PROD}; \
-    echo '✅ generate ${PROD}.bc done'; \
+    cd ..; make -j$(nproc) -C src ${MODIFIED_PROD}; cd -; \
+    echo '✅ compile for Ordinary ${MODIFIED_PROD}: done'; \
+    cd /home/klee/coreutils-${VER}/; clang -fsanitize=address -g -O0 src/${MODIFIED_PROD}.c -o obj-llvm/src/${MODIFIED_PROD}_asan; cd -; \
+    echo '✅ compile for ASan ${MODIFIED_PROD}: done'; \
+    extract-bc ${MODIFIED_PROD}; \
+    echo '✅ generate ${MODIFIED_PROD}.bc done'; \
     exec bash"
